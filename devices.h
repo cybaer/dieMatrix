7dc07842b297a81cac854e800a18d8dbef9e9339
cybaer <cybaer@MacBaer>
2015-02-13 23:55:27 +0100

    new device.h array classes for LEDs and switches

---
+++ devices.h
@@ -0,0 +1,163 @@
+/*
+ * devices.h
+ *
+ *  Created on: 13.02.2015
+ *      Author: cybaer
+ */
+
+#ifndef DEVICES_H_
+#define DEVICES_H_
+
+using namespace avrlib;
+
+#include "mcp23s17.h"
+
+class LED_Base
+{
+public:
+  virtual void init(void) = 0;
+  virtual void set(void) = 0;
+  virtual void clear(void) = 0;
+};
+
+template<typename Extender, uint8_t Port, uint8_t PinNumber>
+class LED : public LED_Base
+{
+  typedef PortPin<Extender, Port, PinNumber> Pin;
+public:
+  LED()
+  {}
+  void init(void)
+  {
+    Pin::set_mode(DIGITAL_OUTPUT);
+  }
+  void set(void) { Pin::set(); }
+  void clear(void) { Pin::clear(); }
+};
+
+class LEDArray
+{
+public:
+  static const int8_t MAX_COUNT = 10;
+  LEDArray(int8_t count, LED_Base* led1, LED_Base* led2, LED_Base* led3, LED_Base* led4, LED_Base* led5=0,
+                         LED_Base* led6=0, LED_Base* led7=0, LED_Base* led8=0, LED_Base* led9=0, LED_Base* led10=0)
+  : m_count(count)
+  {
+    m_LedArray[0] = led1;
+    m_LedArray[1] = led2;
+    m_LedArray[2] = led3;
+    m_LedArray[3] = led4;
+    m_LedArray[4] = led5;
+    m_LedArray[5] = led6;
+    m_LedArray[6] = led7;
+    m_LedArray[7] = led8;
+    m_LedArray[8] = led9;
+    m_LedArray[9] = led10;
+    if(m_count > MAX_COUNT)
+      m_count = MAX_COUNT;
+  }
+  void init(void)
+  {
+    for(int8_t i=0; i<m_count; i++)
+    {
+      m_LedArray[i]->init();
+    }
+  }
+  void clear(void)
+  {
+    for(int8_t i=0; i<m_count; i++)
+    {
+      m_LedArray[i]->clear();
+    }
+  }
+  int8_t m_count;
+  LED_Base* m_LedArray[10];
+
+private:
+  DISALLOW_COPY_AND_ASSIGN(LEDArray);
+};
+
+class SwitchBase
+{
+public:
+  SwitchBase(void)
+  : m_State(0xff)
+  {}
+  virtual void init(void) = 0;
+  virtual uint8_t refresh(void) = 0;
+  bool released() { return m_State == 0x80; }
+  bool pressed()  { return m_State == 0x7f; }
+  bool active()   { return m_State == 0xff; }
+  bool inactive() { return m_State == 0x00; }
+  uint8_t getState() const { return m_State; }
+
+protected:
+  uint8_t m_State;
+};
+
+/* ein Schalter, debounced */
+template<typename Extender, uint8_t Port, uint8_t PinNumber, uint8_t PullUp = 1>
+class Switch : public SwitchBase
+{
+  typedef PortPin<Extender, Port, PinNumber> Pin;
+public:
+  Switch()
+  {}
+  void init(void)
+  {
+    Pin::set_mode(DIGITAL_INPUT);
+    if(PullUp)
+      Pin::setPullUp();
+  }
+  uint8_t refresh()
+  {
+    m_State = (m_State << 1) | (Pin::value() ? PullUp : !PullUp);
+    return m_State;
+  }
+
+private:
+  DISALLOW_COPY_AND_ASSIGN(Switch);
+};
+
+class SwitchArray
+{
+public:
+  static const int8_t MAX_COUNT = 10;
+  SwitchArray(int8_t count, SwitchBase* sw1, SwitchBase* sw2, SwitchBase* sw3, SwitchBase* sw4, SwitchBase* sw5=0,
+                            SwitchBase* sw6=0, SwitchBase* sw7=0, SwitchBase* sw8=0, SwitchBase* sw9=0, SwitchBase* sw10=0)
+  : m_count(count)
+  {
+    m_SwArray[0] = sw1;
+    m_SwArray[1] = sw2;
+    m_SwArray[2] = sw3;
+    m_SwArray[3] = sw4;
+    m_SwArray[4] = sw5;
+    m_SwArray[5] = sw6;
+    m_SwArray[6] = sw7;
+    m_SwArray[7] = sw8;
+    m_SwArray[8] = sw9;
+    m_SwArray[9] = sw10;
+    if(m_count > MAX_COUNT)
+      m_count = MAX_COUNT;
+  }
+  void init(void)
+  {
+    for(int8_t i=0; i<m_count; i++)
+    {
+      m_SwArray[i]->init();
+    }
+  }
+  void refresh(void)
+  {
+    for(int8_t i=0; i<m_count; i++)
+    {
+      m_SwArray[i]->refresh();
+    }
+  }
+  int8_t m_count;
+  SwitchBase* m_SwArray[10];
+
+private:
+  DISALLOW_COPY_AND_ASSIGN(SwitchArray);
+};
+#endif /* DEVICES_H_ */
--- ui.h
+++ ui.h
@@ -7,114 +7,55 @@
 
 #include "avrlib/spi.h"
 #include "avrlib/gpio.h"
-#include "mcp23s17.h"
-#include "avrlib/devices/switch.h"
+#include "devices.h"
 
 using namespace avrlib;
 
 typedef SpiMaster<NumberedGpio<4>, MSB_FIRST, 4> spi_master;
 
 typedef MCP23S17<spi_master, 1> portExtender2;
-//MCP23S17<spi_master, 1> pE2;
 
-//typedef PortX<portExtender2, PORT_B> PortExt2_Out;
-//typedef PortX<portExtender2, PORT_A> PortExt2_In;
+LED<portExtender2, PORT_B, 0> LED_M1;
+LED<portExtender2, PORT_B, 1> LED_M2;
+LED<portExtender2, PORT_B, 2> LED_M3;
+LED<portExtender2, PORT_B, 3> LED_M4;
 
+static LEDArray _4LEDs(4, &LED_M1, &LED_M2, &LED_M3, &LED_M4);
 
+Switch<portExtender2, PORT_A, 7> SW_M1;
+Switch<portExtender2, PORT_A, 6> SW_M2;
+Switch<portExtender2, PORT_A, 5> SW_M3;
+Switch<portExtender2, PORT_A, 4> SW_M4;
 
-typedef PortPin<portExtender2, PORT_B, 0> LED_1;
-typedef PortPin<portExtender2, PORT_B, 1> LED_2;
-typedef PortPin<portExtender2, PORT_B, 2> LED_3;
-typedef PortPin<portExtender2, PORT_B, 3> LED_4;
-
-
-
-
-class SwitchBase
-{
-public:
-  SwitchBase(void)
-  : m_State(0xff)
-  {}
-  virtual void init(void) = 0;
-  virtual uint8_t refresh(void) = 0;
-  bool released() { return m_State == 0x80; }
-  bool pressed()  { return m_State == 0x7f; }
-  bool active()   { return m_State == 0xff; }
-  bool inactive() { return m_State == 0x00; }
-  uint8_t getState() const { return m_State; }
-
-protected:
-  uint8_t m_State;
-};
-
-/* ein Schalter, debounced */
-template<typename Extender, uint8_t Port, uint8_t PinNumber, uint8_t PullUp = 1>
-class Switch : public SwitchBase
-{
-  typedef PortPin<Extender, Port, PinNumber> Pin;
-public:
-  Switch()
-  {}
-  void init(void)
-  {
-    Pin::set_mode(DIGITAL_INPUT);
-    if(PullUp)
-      Pin::setPullUp();
-  }
-  uint8_t refresh()
-  {
-    m_State = (m_State << 1) | (Pin::value() ? PullUp : !PullUp);
-    return m_State;
-  }
-
-private:
-
-  DISALLOW_COPY_AND_ASSIGN(Switch);
-};
-
-Switch<portExtender2, PORT_A, 7> SW_1;
-Switch<portExtender2, PORT_A, 6> SW_2;
-Switch<portExtender2, PORT_A, 5> SW_3;
-Switch<portExtender2, PORT_A, 4> SW_4;
-
-static SwitchBase* _4Switches[] = {&SW_1, &SW_2, &SW_3, &SW_4};
+static SwitchArray _4Switches(4, &SW_M1, &SW_M2, &SW_M3, &SW_M4);
 
 class Ui
 {
 public:
   void Init(void)
   {
-
-    for(int i=0; i<4; i++)
-    {
-      _4Switches[i]->init();
-    }
-    LED_1::set_mode(DIGITAL_OUTPUT);
-    LED_2::set_mode(DIGITAL_OUTPUT);
+    _4Switches.init();
+    _4LEDs.init();
     portExtender2::Init();
   }
 
   void Poll(void)
   {
     portExtender2::ReadIO();
-    SW_1.refresh();
-    SW_2.refresh();
-    SW_3.refresh();
-    SW_4.refresh();
+    _4Switches.refresh();
   }
 
   void Do(void)
   {
-    if(SW_1.active())
-      LED_1::set();
+    if(SW_M1.active())
+      LED_M1.set();
     else
-      LED_1::clear();
+      LED_M1.clear();
 
-    if(SW_4.active())
-      LED_2::set();
+    if(SW_M4.active())
+      LED_M2.set();
     else
-      LED_2::clear();
+      LED_M2.clear();
 
     portExtender2::WriteIO();
   }
